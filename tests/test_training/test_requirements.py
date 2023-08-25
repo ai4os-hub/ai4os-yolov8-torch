510b402246d471bc555e91abe1601f0e687afa9b
khadijeh.alibabaei <khadijeh.alibabaei@kit.edu>
2023-08-25 08:53:02 +0000

    update the test code

--- tests/test_training/test_requirements.py
+++ tests/test_training/test_requirements.py
@@ -19,10 +19,22 @@ the same folder. However, remember to add the prefix `test_` to the file.
 # pylint: disable=unused-argument
 
 
-def test_training_return_type(training):
-    """Tests that training returns a dict type."""
-    assert isinstance(training, dict)
+import os
+import shutil
 
+
+def test_train_function(trained_model_path):
+    checkpoint_file = os.path.join(
+        trained_model_path, "last_model.pth"
+    )
+    assert "last_model.pth" in os.listdir(
+        trained_model_path
+    ), "No checkpoint was saved"
+    assert os.path.exists(
+        checkpoint_file
+    ), "last_model.pth does not exist"
+    # Remove the saved model directory and its contents
+    shutil.rmtree(trained_model_path)
 
 # Example to test training return includes 'end_time'
 # def test_end_time(training):
